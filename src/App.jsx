47de27846fb378dac095295643849bbff5b13642
Aungtayzar <atzphyo1@kmd.edu.mm>
2026-02-12 22:26:02 +0630

    atzp# setup login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import { ToastContainer, Slide } from "react-toastify";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-    </Routes>
-  )
+    <>
+      {/* Routes */}
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+
+      {/* Toast */}
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
