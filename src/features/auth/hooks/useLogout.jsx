3faadcf070183bf04a62574e9fca21d85c7b534b
Htet-Myat-Lin <hlin96741@gmail.com>
2026-02-14 14:58:28 +0630

    hml# Fix Spelling

--- src/features/auth/hooks/useLogout.jsx
+++ src/features/auth/hooks/useLogout.jsx
@@ -12,7 +12,7 @@ export const useLogout = () => {
         onSuccess: () => {
             localStorage.removeItem("access_token")
             setUser(null)
-            navigate("/logout")
+            navigate("/login")
             toast.success("Logout successful!")
         }
     })
